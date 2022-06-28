4ea553345b46dc1c84d0ce40cecc203c13dd29e3
joaoeffting <joaoefftings@gmail.com>
2022-06-28 09:44:15 -0300

    First Commit

---
+++ db.js
@@ -0,0 +1,155 @@
+const mysql = require('mysql2/promise')
+
+async function conectarDB() {
+    const conexao = await mysql.createConnection({
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: '',
+        database: 'controle_comida'
+    })
+
+    return conexao
+}
+
+/*
+    FUNCTION DA TABELA REGISTRO_CONSUMO
+*/
+async function listarComidas() {
+    const conexao = await conectarDB()
+    const sql = 'SELECT RC.ID_CONSUMO, ' +
+                'RC.DATA_CONSUMO, ' +
+                'RC.QUANTIDADE_CONSUMO, ' +
+                'RC.MOTIVO_CONSUMO, ' +
+                'CP.NOME_PRODUTO, ' +
+                'TC.NOME_CONSUMO ' + 
+                'FROM REGISTRO_CONSUMO RC ' +
+                'INNER JOIN CADASTRO_PRODUTO CP ON CP.ID_CADASTRO_PRODUTO = RC.ID_CADASTRO_PRODUTO ' +
+                'INNER JOIN TIPO_CONSUMO TC ON TC.ID_TIPO_CONSUMO = RC.ID_TIPO_CONSUMO '
+    const [comidas] = await conexao.query(sql)
+
+    return comidas
+}
+
+async function listarComidasEntrada() {
+    const conexao = await conectarDB()
+    const sql = 'SELECT RC.ID_CONSUMO, ' +
+                'RC.DATA_CONSUMO, ' +
+                'RC.QUANTIDADE_CONSUMO, ' +
+                'RC.MOTIVO_CONSUMO, ' +
+                'CP.NOME_PRODUTO, ' +
+                'TC.NOME_CONSUMO ' +
+                'FROM REGISTRO_CONSUMO RC ' +
+                'INNER JOIN CADASTRO_PRODUTO CP ON CP.ID_CADASTRO_PRODUTO = RC.ID_CADASTRO_PRODUTO ' +
+                'INNER JOIN TIPO_CONSUMO TC ON TC.ID_TIPO_CONSUMO = RC.ID_TIPO_CONSUMO ' +
+                'WHERE TC.ID_TIPO_CONSUMO = 1 ' +
+                'ORDER BY RC.ID_CONSUMO DESC ' +
+                'LIMIT 4'
+    const [comidas] = await conexao.query(sql)
+
+    return comidas
+}
+
+async function listarComidasSaida() {
+    const conexao = await conectarDB()
+    const sql = 'SELECT RC.ID_CONSUMO, ' +
+                'RC.DATA_CONSUMO, ' +
+                'RC.QUANTIDADE_CONSUMO, ' +
+                'RC.MOTIVO_CONSUMO, ' +
+                'CP.NOME_PRODUTO, ' +
+                'TC.NOME_CONSUMO ' +
+                'FROM REGISTRO_CONSUMO RC ' +
+                'INNER JOIN CADASTRO_PRODUTO CP ON CP.ID_CADASTRO_PRODUTO = RC.ID_CADASTRO_PRODUTO ' +
+                'INNER JOIN TIPO_CONSUMO TC ON TC.ID_TIPO_CONSUMO = RC.ID_TIPO_CONSUMO ' +
+                'WHERE TC.ID_TIPO_CONSUMO = 2 '
+                'ORDER BY TC.ID_TIPO_CONSUMO DESC ' +   
+                'LIMIT 4'
+    const [comidas] = await conexao.query(sql)
+
+    return comidas
+}
+
+async function criarComida(comida) {
+    const conexao = await conectarDB();
+    const sql = 'insert into REGISTRO_CONSUMO (id_cadastro_produto, id_tipo_consumo, quantidade_consumo, motivo_consumo, data_consumo) values (?, ?, ?, ?, ?);';
+
+    return await conexao.query(sql, [comida.produto, comida.consumo, comida.quantidade, comida.motivo, comida.data]);
+}
+
+async function deletarComida(id) {
+    const conexao = await conectarDB();
+    const sql = 'delete from REGISTRO_CONSUMO where ID_CONSUMO = ?;';
+
+    return await conexao.query(sql, [id]);
+}
+
+async function recuperarComida(id) {
+    const conexao = await conectarDB();
+    const sql = 'select * from REGISTRO_CONSUMO where ID_CONSUMO = ?;';
+    const [comida] = await conexao.query(sql, [id]);
+
+    return comida[0];
+}
+
+async function alterarComida(comida) {
+    const conexao = await conectarDB();
+    const sql = 'update REGISTRO_CONSUMO set ID_CADASTRO_PRODUTO = ?, ID_TIPO_CONSUMO = ?, QUANTIDADE_CONSUMO = ?, DATA_CONSUMO = ?, MOTIVO_CONSUMO = ? where ID_CONSUMO = ?;';
+
+    return await conexao.query(sql, [comida.produto, comida.consumo, comida.quantidade, comida.data, comida.motivo, comida.id ])
+}
+
+/*
+    FUNCTION DA TABELA CADASTRO_PRODUTO
+*/
+
+async function listarProdutos() {
+    const conexao = await conectarDB();
+    const sql = 'SELECT * FROM CADASTRO_PRODUTO;'
+    const [produtos] = await conexao.query(sql)
+
+    return produtos
+}
+
+async function criarProduto(produto) {
+    const conexao = await conectarDB();
+    const sql = 'INSERT INTO CADASTRO_PRODUTO (NOME_PRODUTO) VALUES (?);'
+    
+    return await conexao.query(sql, [produto.produto]);
+}
+
+async function deletarProduto(id) {
+    const conexao = await conectarDB();
+    const sql = 'delete from CADASTRO_PRODUTO where ID_CADASTRO_PRODUTO = ?;';
+
+    return await conexao.query(sql, [id]);
+}
+
+async function recuperarProduto(id) {
+    const conexao = await conectarDB();
+    const sql = 'select * from CADASTRO_PRODUTO where ID_CADASTRO_PRODUTO = ?;';
+    const [produto] = await conexao.query(sql, [id]);
+
+    return produto[0];
+}
+
+async function alterarProduto(produto) {
+    const conexao = await conectarDB();
+    const sql = 'update CADASTRO_PRODUTO set NOME_PRODUTO = ? where ID_CADASTRO_PRODUTO = ?;';
+
+    return await conexao.query(sql, [produto.produto, produto.id ])
+}
+
+module.exports = { 
+    listarComidas,
+    listarComidasEntrada,
+    listarComidasSaida,
+    listarProdutos,
+    criarComida,
+    criarProduto,
+    deletarComida,
+    deletarProduto,
+    recuperarComida,
+    recuperarProduto,
+    alterarComida,
+    alterarProduto
+}
---
+++ routes/index.js
@@ -0,0 +1,116 @@
+var express = require('express');
+var router = express.Router();
+
+// CRUD DO REGISTRO_CONSUMO
+/*  LISTAR OS REGISTROS NAS PÁGINAS */
+router.get('/', async function(req, res) {
+  const entrada = await global.db.listarComidasEntrada();
+  const saida = await global.db.listarComidasSaida();
+  res.render('index', {entrada, saida});
+})
+
+router.get('/indexComida', async function(req, res) {
+  const data = await global.db.listarComidas();
+  res.render('comidas/index', {data});
+})
+
+router.get('/indexComidaEntrada', async function(req, res) {
+  const data = await global.db.listarComidasEntrada();
+  res.render('comidas/index', {data});
+})
+
+router.get('/indexComidaSaida', async function(req, res) {
+  const data = await global.db.listarComidasSaida();
+  res.render('comidas/index', {data});
+})
+
+/* GET FORM PAGE COMIDA*/
+router.get('/newComida', async function(req, res) {
+  res.render('comidas/form', { title: 'New Comida', acao: '/newComida', comida: {} });
+})
+
+/* CRIAR NOVO REGISTRO NA TABELA REGISTRO_CONSUMO */
+router.post('/newComida', async function(req, res) {
+  const produto = req.body.produto;
+  const consumo = req.body.consumo;
+  const quantidade = parseInt(req.body.quantidade);
+  const motivo = req.body.motivo;
+  const data = req.body.data;
+
+  await global.db.criarComida({ produto, consumo, quantidade, motivo, data });
+  res.redirect('/indexComida');
+})
+
+/* DELETAR REGISTRO DA TABELA REGISTRO_CONSUMO */ 
+router.get('/deletarComida/:id', async function (req, res) {
+  const id = parseInt(req.params.id);
+  await global.db.deletarComida(id);
+  
+  res.redirect('/indexComida');
+})
+
+/* RECUPARAR OS DADOS DO REGISTRO_CONSUMO E COLOCAR NO FORMULÁRIO */
+router.get('/alterarComida/:id', async function (req, res) {
+  const id = parseInt(req.params.id);
+  const comida = await global.db.recuperarComida(id);
+
+  res.render('comidas/form', { title: 'Alterar Comida', acao: '/alterarComida/' + id, comida });
+})
+
+/* ALTERAR OS DADOS NO REGISTRO_CONSUMO */
+router.post('/alterarComida/:id', async function(req, res) {
+  const id = parseInt(req.params.id);
+  const produto = parseInt(req.body.produto);
+  const consumo = parseInt(req.body.consumo); 
+  const quantidade = parseInt(req.body.quantidade);
+  const motivo = req.body.motivo;
+  const data = req.body.data;
+
+  await global.db.alterarComida({ id, produto, consumo, quantidade, motivo, data });
+  res.redirect('/indexComida');
+})
+
+// ------------------------------------------------------------------------------------------------------------------------------------ //
+
+/* GET home page Produto */
+router.get('/indexProduto', async function(req, res) {
+  const produto = await global.db.listarProdutos();
+  res.render('produto/index', {produto});
+})
+
+/* GET form page Produto */
+router.get('/newProduto', async function(req, res) {
+  res.render('produto/form', {title: 'New Produto', acao: '/newProduto', produto: {} });
+})
+
+/* POST NEW PRODUTO */
+router.post('/newProduto', async (req, res) => {
+  const produto = req.body.produto;
+
+  await global.db.criarProduto({ produto });
+  res.redirect('/indexComida');
+})
+
+router.get('/deletarProduto/:id', async function (req, res) {
+  const id = parseInt(req.params.id);
+  await global.db.deletarProduto(id);
+
+  res.redirect('/indexProduto');
+})
+
+router.get('/alterarProduto/:id', async function (req, res) {
+  const id = parseInt(req.params.id);
+  const produto = await global.db.recuperarProduto(id);
+
+  res.render('produto/form', { title: 'Alterar Produto', acao: '/alterarProduto/' + id, produto });
+})
+
+router.post('/alterarProduto/:id', async function(req, res) {
+  const id = parseInt(req.params.id);
+  const produto = req.body.produto;
+
+  await global.db.alterarProduto({ id, produto });
+  res.redirect('/indexProduto');
+})
+
+module.exports = router;
